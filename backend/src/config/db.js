84a6b7330311618fea889e213599db170c5ff51a
Gonzalo Valenzuela <gvalenzuelac@live.com>
2024-11-22 16:51:25 -0300

    deploy primer intento

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'postgres',        // Reemplaza con tu usuario de PostgreSQL
-  host: 'localhost',
-  database: 'finaltest', // Reemplaza con el nombre de tu base de datos
-  password: 'LmGl1993', // Reemplaza con tu contraseña de PostgreSQL
-  port: 5432,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
 });
 
 module.exports = pool;
