187b328b7c2120c39a3b8802ab3d09fcb59931f7
Gonzalo Valenzuela <gvalenzuelac@live.com>
2024-11-22 18:30:13 -0300

    url pública

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
 
+// Usa la URL pública de la base de datos de Railway
 const pool = new Pool({
-  host: process.env.PGHOST,
-  port: process.env.PGPORT,
-  user: process.env.PGUSER,
-  password: process.env.PGPASSWORD,
-  database: process.env.PGDATABASE,
+  connectionString: process.env.DATABASE_PUBLIC_URL, 
+  ssl: {
+    rejectUnauthorized: false,  // Esto desactiva la verificación del certificado SSL si es necesario
+  },
 });
 
 module.exports = pool;
