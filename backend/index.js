1dd2607035438392c21d44d1aacbdcfee335982e
Gonzalo Valenzuela <gvalenzuelac@live.com>
2024-11-22 21:03:02 -0300

    verifica

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,14 @@
 require('dotenv').config();
 const app = require('./src/app');
-console.log(process.env.JWT_SECRET);
+
+// Verificar que JWT_SECRET esté cargado correctamente
+if (!process.env.JWT_SECRET) {
+  console.error('JWT_SECRET no está definido en las variables de entorno');
+  process.exit(1); // Detener el servidor si no está configurada la clave secreta
+}
+
+console.log('JWT_SECRET:', process.env.JWT_SECRET);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
