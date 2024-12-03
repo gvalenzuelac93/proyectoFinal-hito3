ee1ea7233b3f8bdd23a9a4bbf2a4d372674a5143
Gonzalo Valenzuela <gvalenzuelac@live.com>
2024-12-03 18:09:27 -0300

    auth

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -1,17 +1,29 @@
 const jwt = require('jsonwebtoken');
 
 const verificarToken = async (req, res, next) => {
+    // Obtener el token del encabezado Authorization
     const token = req.header('Authorization')?.replace('Bearer ', '');
-    
+
+    // Verificar si el token fue proporcionado
     if (!token) {
         return res.status(401).json({ error: 'Token no proporcionado' });
     }
 
     try {
+        // Verificar y decodificar el token
         const decoded = await jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; // Almacena la información del usuario decodificada
+        
+        // Almacenar la información del usuario decodificada en req.user
+        req.user = {
+            id: decoded.id, 
+            email: decoded.email,
+            rol: decoded.rol
+        };
+
+        // Continuar con la siguiente función de middleware
         next();
     } catch (error) {
+        // Manejo de errores si el token es inválido
         return res.status(401).json({ error: 'Token inválido' });
     }
 };
